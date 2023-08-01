3d6c4581a5ab6464307f0ddfcb9ae06b020a60ec
AluHadj22 <140946881+AluHadj22@users.noreply.github.com>
2023-08-01 07:52:56 +0300

    Update create_bot.py

--- create_bot.py
+++ create_bot.py
@@ -1,9 +1,9 @@
 from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.fsm_storage.memory import MemoryStorage # Чтобы хранить в памяти ответы от машины состояний.
 
 storage = MemoryStorage()
 
 TOKEN = 'YOUR TOKEN'
 bot = Bot(TOKEN)
-dp = Dispatcher(bot, storage=storage)
+dp = Dispatcher(bot, storage=storage) # обязательно нужно указать storage для работы.
